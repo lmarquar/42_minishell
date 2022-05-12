2ae2662e46f309742da40d261be684d9625ac135
lmarquar <leon.marquardt@gmx.de>
2022-05-12 16:07:31 +0200

    dude this rocks

---
+++ executer/exec_with_pipes.c
@@ -0,0 +1,125 @@
+#include "execute.h"
+
+int	exec_in_to_pipe(t_bin *bin, int *pid, int fd[], size_t (*i)[])
+{
+	if (pipe(&(fd[0])) == -1)
+		return (3);
+	if (bin->cmd_line->heredoc_delimiter)
+	{
+		heredoc_handler(bin->cmd_line, fd[1]);
+		(*i)[1] = (*i)[1] - 1;
+	}
+	else if (bin->cmd_line->simple_commands[0]->is_builtin)
+	{
+		exec_builtin(bin, bin->cmd_line->simple_commands[0]->args, fd[4], fd[1]);
+		bin->cmd_line->simple_commands = bin->cmd_line->simple_commands + 1;
+		(*i)[1] = (*i)[1] - 1;
+	}
+	else
+	{
+		pid[**i] = fork();
+		if (pid[**i] == 0)
+		{
+			close(fd[0]);
+			exec_el(bin->cmd_line->simple_commands[0]->args, bin, fd[4], fd[1]);
+		}
+		bin->cmd_line->simple_commands = bin->cmd_line->simple_commands + 1;
+		(**i)++;
+	}
+	return (0);
+}
+
+int	exec_pipe1_to_pipe2(t_bin *bin, int *pid, int fd[], size_t (*i)[])
+{
+	close(fd[1]);
+	if (pipe(&(fd[2])) == -1)
+		return (3);
+	if (bin->cmd_line->simple_commands[0]->is_builtin)
+	{
+		exec_builtin(bin, bin->cmd_line->simple_commands[0]->args, fd[0], fd[3]);
+		bin->cmd_line->simple_commands = bin->cmd_line->simple_commands + 1;
+		(*i)[1] = (*i)[1] - 1;
+	}
+	else
+	{
+		pid[**i] = fork();
+		if (pid[**i] == 0)
+		{
+			close(fd[2]);
+			exec_el(bin->cmd_line->simple_commands[0]->args, bin, fd[0], fd[3]);
+		}
+		bin->cmd_line->simple_commands = bin->cmd_line->simple_commands + 1;
+		(**i)++;
+	}
+	close(fd[0]);
+	return (0);
+}
+
+int	exec_pipe2_to_pipe1(t_bin *bin, int *pid, int fd[], size_t (*i)[])
+{
+	if (pipe(&(fd[0])) == -1)
+		return (3);
+	close(fd[3]);
+	if (bin->cmd_line->simple_commands[0]->is_builtin)
+	{
+		exec_builtin(bin, bin->cmd_line->simple_commands[0]->args, fd[2], fd[1]);
+		bin->cmd_line->simple_commands = bin->cmd_line->simple_commands + 1;
+		(*i)[1] = (*i)[1] - 1;
+	}
+	else
+	{
+		pid[**i] = fork();
+		if (pid[**i] == 0)
+		{
+			close(fd[0]);
+			exec_el(bin->cmd_line->simple_commands[0]->args, bin, fd[2], fd[1]);
+		}
+		bin->cmd_line->simple_commands = bin->cmd_line->simple_commands + 1;
+		(**i)++;
+	}
+	close(fd[2]);
+	return (0);
+}
+
+int	exec_pipe_to_out(t_bin *bin, int *pid, int fd[], size_t (*i)[])
+{
+	int	i_fd;
+
+	i_fd = 0;
+	if (bin->cmd_line->pipe_count % 2)
+		i_fd = 2;
+	close(fd[1 + i_fd]);
+	if (bin->cmd_line->append > 0)
+	{
+		append(fd[i_fd], fd[5]);
+		((*i)[1])--;
+	}
+	else if (bin->cmd_line->simple_commands[0]->is_builtin)
+		exec_builtin(bin, bin->cmd_line->simple_commands[0]->args, fd[i_fd], fd[5]);
+	else
+	{
+		pid[**i] = fork();
+		if (pid[**i] == 0)
+			exec_el(bin->cmd_line->simple_commands[0]->args, bin, fd[i_fd], fd[5]);
+	}
+	close(fd[i_fd]);
+	return (0);
+}
+
+int	exec_with_pipes(t_bin *bin, int *pid, int fd[])
+{
+	size_t	i[2];
+
+	i[0] = 0;
+	i[1] = bin->cmd_line->pipe_count;
+	exec_in_to_pipe(bin, pid, fd, &i);
+	while (i[0] < i[1])
+	{
+		exec_pipe1_to_pipe2(bin, pid, fd, &i);
+		if (i[0] >= i[1])
+			break ;
+		exec_pipe2_to_pipe1(bin, pid, fd, &i);
+	}
+	exec_pipe_to_out(bin, pid, fd, &i);
+	return (0);
+}
--- executer/execute.c
+++ executer/execute.c
@@ -30,166 +30,37 @@ int	execute_init_vars(t_cmd_line **cmd_line, int (*fd)[], int **pid)
 	return (0);
 }
 
-int ft_strcmp(char *s1, char *s2)
-{
-	while (*s1 == *s2 && *s1 && *s2)
-	{
-		s1++;
-		s2++;
-	}
-	if (!(*s1) && !(*s2))
-		return (1);
-	return (0);
-}
-
-int	exec_builtin(char **args, int fdin, int fdout)
-{
-	if (ft_strcmp(args[0], "cd"))
-		exec_cd(args[1]);
-
-	//currently unused:
-	fdin++;
-	fdout++;
-	return (0);
-}
-
-int exec_in_to_out(t_bin *bin, int *pid, int fd[])
+int	exec_in_to_out(t_bin *bin, int *pid, int fd[])
 {
 	if (bin->cmd_line->simple_commands[0]->is_builtin)
-			exec_builtin(bin->cmd_line->simple_commands[0]->args, fd[4], fd[5]);
+		exec_builtin(bin, bin->cmd_line->simple_commands[0]->args, fd[4], fd[5]);
 	else
 	{
-		pid[0] = fork();
-		if (pid[0] == 0)
-			exec_el(bin->cmd_line->simple_commands[0]->args, bin->paths, fd[4], fd[5]);
+		*pid = fork();
+		if (*pid == 0)
+			exec_el(bin->cmd_line->simple_commands[0]->args, bin, fd[4], fd[5]);
 	}
-	return (0);
-}
-
-int	exec_in_to_pipe(t_bin *bin, int *pid, int fd[], size_t *i)
-{
-		if (pipe(&(fd[0])) == -1)
-			return (3);
-		if (bin->cmd_line->heredoc_delimiter)
-			heredoc_handler(bin->cmd_line, fd[1]);
-		else
-		{
-			pid[*i] = fork();
-			if (pid[*i] == 0)
-			{
-				close(fd[0]);
-				exec_el(bin->cmd_line->simple_commands[*i]->args, bin->paths, fd[4], fd[1]);
-			}
-		}
-		(*i)++;
-		return (0);
-}
-
-int	exec_pipe1_to_pipe2(t_bin *bin, int *pid, int fd[], size_t *i)
-{
-	if (pipe(&(fd[2])) == -1)
-		return (3);
-	pid[*i] = fork();
-	if (pid[*i] == 0)
-	{
-		close(fd[2]);
-		exec_el(bin->cmd_line->simple_commands[*i]->args, bin->paths, fd[0], fd[3]);
-	}
-	close(fd[0]);
-	close(fd[3]);
-	(*i)++;
-	return (0);
-}
-
-int	exec_pipe2_to_pipe1(t_bin *bin, int *pid, int fd[], size_t *i)
-{
-	if (pipe(&(fd[0])) == -1)
-		return (3);
-	pid[*i] = fork();
-	if (pid[*i] == 0)
-	{
-		close(fd[0]);
-		exec_el(bin->cmd_line->simple_commands[*i]->args, bin->paths, fd[2], fd[1]);
-	}
-	close(fd[2]);
-	close(fd[1]);
-	(*i)++;
-	return (0);
-}
-
-int	exec_pipe1_to_out(t_bin *bin, int *pid, int fd[], size_t *i)
-{
-	if (bin->cmd_line->append > 0)
-		append(fd[0], fd[5]);
-	else
-	{
-		pid[*i] = fork();
-		if (pid[*i] == 0)
-			exec_el(bin->cmd_line->simple_commands[*i]->args, bin->paths, fd[0], fd[5]);
-	}
-	close(fd[0]);
-	return (0);
-}
-
-int	exec_pipe2_to_out(t_bin *bin, int *pid, int fd[], size_t *i)
-{
-	if (bin->cmd_line->append > 0)
-		append(fd[2], fd[5]);
-	else
-	{
-		pid[*i] = fork();
-		if (pid[*i] == 0)
-			exec_el(bin->cmd_line->simple_commands[*i]->args, bin->paths, fd[2], fd[5]);
-	}
-	close(fd[2]);
-	return (0);
-}
-
-int	exec_with_pipes(t_bin *bin, int *pid, int fd[])
-{
-	size_t	i;
-
-	i = 0;
-	exec_in_to_pipe(bin, pid, fd, &i);
-	if (bin->cmd_line->heredoc_delimiter)
-		i--;
-	while ((i < bin->cmd_line->pipe_count && !bin->cmd_line->heredoc_delimiter) || i < (bin->cmd_line->pipe_count - 1))
-	{
-		exec_pipe1_to_pipe2(bin, pid, fd, &i);
-		if ((i >= bin->cmd_line->pipe_count && !(bin->cmd_line->heredoc_delimiter)) || i >= (bin->cmd_line->pipe_count - 1))
-			break ;
-		exec_pipe2_to_pipe1(bin, pid, fd, &i);
-	}
-	if (bin->cmd_line->append > 0)
-		i--;
-	if ((bin->cmd_line->pipe_count % 2))
-		exec_pipe1_to_out(bin, pid, fd, &i);
-	else
-		exec_pipe2_to_out(bin, pid, fd, &i);
 	return (0);
 }
 
 int	execute(t_bin *bin)
 {
-	int		*pid;
-	int		fd[6];
-	int		error_code;
-	int		k;
+	int	*pid;
+	int	fd[6];
+	int	exit_code;
 
 	execute_init_vars(&bin->cmd_line, &fd, &pid);
 	if (!bin->cmd_line->pipe_count)
 		exec_in_to_out(bin, pid, fd);
 	else
 		exec_with_pipes(bin, pid, fd);
-	k = -1;
-	while (pid[++k])
+	while (*pid)
 	{
-		waitpid(pid[k], &error_code, 0);
-		if (error_code != 0 && error_code != 256)
+		waitpid(*pid, &exit_code, 0);
+		printf("PID: %d, exit_code: %d\n", *pid, exit_code);
+		if (exit_code != 0 && exit_code != 256)
 			perror("program failed");
-		else
-			printf("PID: %d, EXIT_CODE: %d\n", pid[k], error_code);
+		pid++;
 	}
-	free(pid);
 	return (0);
 }
