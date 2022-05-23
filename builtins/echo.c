fcf24bbb4b151638b65d7614e14a34e648f84820
chelmerd <chelmerd@student.42wolfsburg.de>
2022-05-23 15:14:42 +0200

    echo: multiple -n, nothing, write to fdout, doku

--- builtins/echo.c
+++ builtins/echo.c
@@ -1,22 +1,32 @@
 #include "builtins.h"
 
+/**
+ * @brief writes the arguments to the given filedescriptor
+ * 
+ * @param fdout default stdout
+ * @param args options (-n for no newline) and arguments
+ * @return always int 0
+ */
 int	exec_echo(int fdout, char **args)
 {
 	int		i;
 
 	i = 1;
 	if (!args[i])
+	{
+		write(fdout, "\n", 1);
 		return (0);
-	if (!ft_strncmp(args[i], "-n", 2))
+	}
+	while (!ft_strncmp(args[i], "-n", 3))
 		i++;
 	while (args[i])
 	{
 		write(fdout, args[i], ft_strlen(args[i]));
 		i++;
 		if (args[i])
-			write(1, " ", 1);
+			write(fdout, " ", 1);
 	}
 	if (ft_strncmp(args[1], "-n", 2))
-		write(1, "\n", 1);
+		write(fdout, "\n", 1);
 	return (0);
 }
