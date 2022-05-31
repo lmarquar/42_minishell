2953c40327af616be803594b0ca83843c44f9915
Chris Helmerding <chelmerd@student.42wolfsburg.de>
2022-05-31 13:32:08 +0200

    fix exit: exit in pipeline without arg

--- builtins/exit.c
+++ builtins/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chelmerd <chelmerd@student.42wolfsburg.    +#+  +:+       +#+        */
+/*   By: chelmerd <chelmerd@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 12:52:54 by lmarquar          #+#    #+#             */
-/*   Updated: 2022/05/31 11:17:26 by chelmerd         ###   ########.fr       */
+/*   Updated: 2022/05/31 12:49:10 by chelmerd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,14 @@ static int		is_numeric(const char *s);
 static void		clean_up(t_bin *bin);
 static void		clean_exit(t_bin *bin, int exit_code);
 
+/**
+ * @brief exits the shell and returning the last exit_code or the first arg
+ *
+ * @param bin shell variables/state
+ * @param args arguments: "exit" [n] - n is checked to be numeric
+ * @param o_err_msg only error message: only check for errors but dont exit
+ * @return int 2 if first argument is not numeric, 1 if too many arguments
+ */
 int	exec_exit(t_bin *bin, char **args, int o_err_msg)
 {
 	int	error;
@@ -25,18 +33,23 @@ int	exec_exit(t_bin *bin, char **args, int o_err_msg)
 	if (!o_err_msg)
 		write(1, "exit\n", 5);
 	exit_code = bin->exit_code;
-	if ((!args || args[1] == NULL) && !o_err_msg)
+	if ((!args || !args[1]) && !o_err_msg)
 		clean_exit(bin, exit_code);
+	if ((!args || !args[1]) && o_err_msg)
+		return (exit_code);
 	if (!is_numeric(args[1]))
-		clean_exit(bin, builtin_error(2, "exit", "numeric argument required"));
+	{
+		error = builtin_error(2, "exit", "numeric argument required");
+		if (o_err_msg)
+			return (error);
+		clean_exit(bin, error);
+	}
 	error = ft_atoi(args[1]) % 256;
 	if (args[2] && args[2][0])
-	{
 		return (builtin_error(1, "exit", "too many arguments"));
-	}
 	if (!o_err_msg)
 		clean_exit(bin, error);
-	return (exit_code);
+	return (error);
 }
 
 static void	clean_exit(t_bin *bin, int exit_code)
